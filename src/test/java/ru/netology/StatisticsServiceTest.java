c5f74191139ff0905704ad9413fbbf93d8abfa2c
Светлана Чернова <lana_19.87@mail.ru>
2022-04-12 13:17:59 +0600

    Add 100 coverage

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -21,13 +21,23 @@ public class StatisticsServiceTest {
     void findMax1() {
         StatisticService service = new StatisticService();
 
-        long[] incomesInBillions = {12, 5, 9, 3, 2, 8, 10, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 9, 3, 2, 15, 10, 6, 11, 11, 12};
+        long expected = 15;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMax2() {
+        StatisticService service = new StatisticService();
 
+        long[] incomesInBillions = {19, 5, 19, 3, 1, 5, 1, 6, 2, 3, 12};
+        long expected = 19;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 
 }
